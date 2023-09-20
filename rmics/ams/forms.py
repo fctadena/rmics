dda4e8f1e00122233860f7979d8ba4b8aedf76f0
Francis Carlo Tadena <franciscarlotadena@yahoo.com.ph>
2023-09-20 22:35:29 +0800

    Fixing Add asset form not posting

--- rmics/ams/forms.py
+++ rmics/ams/forms.py
@@ -4,19 +4,104 @@ from .models import Asset
 
 
 class AssetForm(forms.ModelForm):
-    class Meta:
-        model = Asset
-        fields = ['asset_manufacturer', 'asset_model', 'asset_rated_capacity', 'asset_type', 'asset_sub_type', 'asset_drive_details', 'asset_year', 'asset_month', 'asset_image_primary']
+    
+    
         
-        widgets = {
-            'asset_manufacturer': forms.TextInput(attrs={'class': 'form-control'}),
-            'asset_model': forms.TextInput(attrs={'class': 'form-control'}),
-            'asset_rated_capacity': forms.TextInput(attrs={'class': 'form-control'}),
-            'asset_type': forms.TextInput(attrs={'class': 'form-control'}),
-            'asset_sub_type': forms.TextInput(attrs={'class': 'form-control'}),
-            'asset_drive_details': forms.TextInput(attrs={'class': 'form-control'}),
-            'asset_year': forms.TextInput(attrs={'class': 'form-control'}),
-            'asset_month': forms.TextInput(attrs={'class': 'form-control'}),
-            'asset_image_primary': forms.FileInput(attrs={'class': 'form-control', 'id':'inputGroupFile01'}),
-        }
+    # widgets = {
+    #             'asset_manufacturer': forms.TextInput(attrs={'class': 'input', 'type': 'text', 'placeholder': ''}),
+    #             'asset_model': forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),
+    #             'asset_rated_capacity': forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),
+    #             'asset_type': forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),
+    #             'asset_sub_type': forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),
+    #             'asset_drive_details': forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),
+    #             'asset_year': forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),
+    #             'asset_month': forms.TextInput(attrs={'class': 'input', 'placeholder': ''}),
+    #             'asset_image_primary': forms.FileInput(attrs={'class': 'input', 'id':'inputGroupFile01'}),
+    #         }
+    
+    
+    class Meta:
+            model = Asset
+            fields = ['asset_manufacturer', 'asset_model', 'asset_rated_capacity', 'asset_type', 'asset_sub_type', 'asset_drive_details', 'asset_year', 'asset_month', 'asset_image_primary']
+      
+        
+    asset_manufacturer = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "input",
+        "type": "text",
+        "placeholder": "",
+    }))
+    
+    
+    asset_model = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "input",
+        "type": "text",
+        "placeholder": "",
+    }))
+
+    asset_rated_capacity = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "input",
+        "type": "text",
+        "placeholder": "",
+    }))
+
+    asset_type = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "input",
+        "type": "text",
+        "placeholder": "",
+    }))
+
+    asset_sub_type = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "input",
+        "type": "text",
+        "placeholder": "",
+    }))
+
+    asset_drive_details = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "input",
+        "type": "text",
+        "placeholder": "",
+    }))
+
+    asset_year = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "input",
+        "type": "text",
+        "placeholder": "",
+    }))
+
+    asset_month = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "input",
+        "type": "text",
+        "placeholder": "",
+    }))
+
+    
+    asset_image_primary = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "input",        
+        "type": "file",
+        "data-role": "magic-overlay",
+        "data-target": "#pictureBtn",
+        "data-edit": "insertImage",
+    }))
+    
+    
+    # widgets = {
+    #         'asset_manufacturer': forms.CharField(attrs={'class': 'input', 'type': 'text', 'placeholder': ''}),
+    #         'asset_model': forms.CharField(attrs={'class': 'input', 'type': 'text', 'placeholder': ''}),
+    #         'asset_rated_capacity': forms.CharField(attrs={'class': 'input', 'type': 'text', 'placeholder': ''}),
+    #         'asset_type': forms.CharField(attrs={'class': 'input', 'type': 'text', 'placeholder': ''}),
+    #         'asset_sub_type': forms.CharField(attrs={'class': 'input', 'type': 'text', 'placeholder': ''}),
+    #         'asset_drive_details': forms.CharField(attrs={'class': 'input', 'type': 'text', 'placeholder': ''}),
+    #         'asset_year': forms.CharField(attrs={'class': 'input', 'type': 'text', 'placeholder': ''}),
+    #         'asset_month': forms.CharField(attrs={'class': 'input', 'type': 'text', 'placeholder': ''}),
+    #         'asset_image_primary': forms.FileInput(attrs={'class': 'input', 'id':'inputGroupFile01'}),
+    #     }
+        
+
+        
+       
+       
+       
+       
+       
+       
         
