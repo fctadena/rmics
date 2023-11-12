9ee598b90f008db53739798287f4c2540ee1b307
Francis Carlo Tadena <franciscarlotadena@yahoo.com.ph>
2023-11-12 20:38:58 +0800

    Continue DRMS CRUD

--- rmics/drms/urls.py
+++ rmics/drms/urls.py
@@ -8,11 +8,10 @@ app_name = 'drms'
 
 urlpatterns = [
     path('', views.drms, name='drms_index'),
-    path('daily-report/', views.daily_report, name='daily_report'),
-    path('log-detail/', views.log_detail, name='log_detail'),
+    path('log-detail/<int:pk>/', views.log_detail.as_view(), name='log_detail'),
     path('add-log/', views.add_log.as_view(), name='add_log'),
-    path('delete-log/', views.delete_log, name='delete_log'),
-    #path('update-log/', views.update_log, name='update_log'),
-    path('update-log/', views.update_records_view, name='update_log'),
+    path('delete-log/<int:id>/', views.delete_log, name='delete_log'),
+    path('update-log/', views.update_log, name='update_log'),
+    # path('update-log/', views.update_records_view, name='update_log'),
     path('maintenance-records/', views.maintenance_records, name='maintenance_records'),
 ]
--- rmics/drms/views.py
+++ rmics/drms/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import CreateView
 from .forms import MaintenanceLogForm
 from django.contrib import messages
 from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
 
 #IMPORTS FOR THE HANDLER
 from django.views import View
@@ -15,39 +16,33 @@ from ams.models import Asset
 
 # Create your views here.
 
-
 def drms(request):
     return render(request, 'rmics/drms-base.html')
 
 
-def daily_report(request):
-    return render(request, 'drms/daily-report.html')
-
-def log_detail(request):
-    return render(request, 'drms/log-detail.html')
-
-
-#ADDING LOG
+#CRUD STARTS HERE
+class log_detail(DetailView):
+    model = MaintenanceLog
+    template_name = 'drms/log-detail.html'
 
 
-def delete_log(request):
-    return render(request, 'drms/delete-log.html')
+def delete_log(request,id):
+    log = MaintenanceLog.objects.get(id=id)
+    
+    if request.method == 'POST':
+        log.delete()
+        messages.success(request, 'DELETED LOG SUCCESSFULLY', extra_tags='warning')
+        return redirect('drms:maintenance_records')
+    
+    return render(request, 'drms/delete-log.html', {'log':log})
+
 
 def update_log(request):
     return render(request, 'drms/update-log.html')
 
-
-def maintenance_records(request):
-    model = MaintenanceLog
-    
-    return render(request, 'drms/maintenance-records.html', )
-
 def maintenance_records(request):
     maintenance_log = MaintenanceLog.objects.all()
     return render(request, 'drms/maintenance-records.html', {'maintenance_log':maintenance_log})
-
-
-
 
 
 class add_log(CreateView):
@@ -59,26 +54,26 @@ class add_log(CreateView):
         messages.success(self.request, 'ADDED LOG SUCCESSFULLY', extra_tags='success')
         return reverse_lazy('drms:maintenance_records')
     
-    
+
 #ERROR HANDLER
-def update_records_view(request):
-    template_name = 'drms/update-log.html'
+# def update_records_view(request):
+#     template_name = 'drms/update-log.html'
 
-    if request.method == 'GET':
-        return render(request, template_name)
-    elif request.method == 'POST':
-        try:
-            # Update logic
-            maintenance_logs = MaintenanceLog.objects.filter(equipment_name__isnull=False)
+#     if request.method == 'GET':
+#         return render(request, template_name)
+#     elif request.method == 'POST':
+#         try:
+#             # Update logic
+#             maintenance_logs = MaintenanceLog.objects.filter(equipment_name__isnull=False)
             
-            for log in maintenance_logs:
-                log.equipment_name = None
-                log.save()
+#             for log in maintenance_logs:
+#                 log.equipment_name = None
+#                 log.save()
 
-            # Display success message
-            messages.success(request, 'Records updated successfully.')
-        except Exception as e:
-            # Display error message if update fails
-            messages.error(request, f'An error occurred: {str(e)}')
+#             # Display success message
+#             messages.success(request, 'Records updated successfully.')
+#         except Exception as e:
+#             # Display error message if update fails
+#             messages.error(request, f'An error occurred: {str(e)}')
 
-        return redirect('drms:update_log')
+#         return redirect('drms:update_log')
