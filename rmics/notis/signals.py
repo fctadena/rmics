53400a93515abd593b3d3ba8da852be3024db0a0
Francis Carlo Tadena <franciscarlotadena@yahoo.com.ph>
2024-01-21 21:58:37 +0800

    Continue signals

--- rmics/notis/signals.py
+++ rmics/notis/signals.py
@@ -91,21 +91,31 @@ def new_asset_added(instance, created, **kwargs):
 @receiver(post_save, sender=MaintenanceLog)            
 def critical_findings_created(instance, created, **kwargs):
     print("Signal received!")
-    if created or instance.work_type == "Repair":
-        #CONTINUE HERE! ADD LOGIC IF LOG IS FOR A CRITICAL EQUIPMENT
-        if instance.equipment_name.critical_asset_designation:
-            print("Signal for Corrective Maintenance Log for a Critical Asset Triggered")
-            # Assuming instance.log_reporter is the user you want to notify
-            # Modify this logic based on your actual requirements
-            recipient_users = []
+    if created and instance.work_type == "Repair" and instance.equipment_name.critical_asset_designation == True or (instance.work_type == "Repair" and instance.equipment_name.critical_asset_designation == True):
+        print("Signal for Corrective Maintenance Log for a Critical Asset Triggered")
+        recipient_users = []
 
-            # Check user's group and add to recipient_users accordingly
-            for group_name in ['Operations Analyst Super', 'Operations Analyst', 'National Management']:
-                group = Group.objects.get(name=group_name)
-                users_in_group = group.user_set.all()
-                recipient_users.extend(users_in_group)
+        for group_name in ['Operations Analyst Super', 'Operations Analyst', 'National Management']:
+            group = Group.objects.get(name=group_name)
+            users_in_group = group.user_set.all()
+            recipient_users.extend(users_in_group)
 
-            # Send notification to the specified group
-            notify.send(sender=instance, recipient=recipient_users, verb="New Corrective Maintenance Log for a CRITICAL ASSET ADDED")
-            print("New Corrective Maintenance Log for a CRITICAL ASSET ADDED")
+        notify.send(sender=instance, recipient=recipient_users, verb="New Corrective Maintenance Log for a CRITICAL ASSET ADDED")
+        print("New Corrective Maintenance Log for a CRITICAL ASSET ADDED")
     
+    
+# A MAINTENANCE LOG WITH AFFECTING BAGGING AS TRUE
+@receiver(post_save, sender=MaintenanceLog)            
+def maintenance_log_affecting_bagging(instance, created, **kwargs):
+    print("Signal received!")
+    if created and instance.affecting_bagging == True or (instance.affecting_bagging == True):
+        print("Signal for MAINTENANCE LOG WITH AFFECTING BAGGING AS TRUE Triggered")
+        recipient_users = []
+
+        for group_name in ['Operations Analyst Super', 'Operations Analyst', 'National Management']:
+            group = Group.objects.get(name=group_name)
+            users_in_group = group.user_set.all()
+            recipient_users.extend(users_in_group)
+
+        notify.send(sender=instance, recipient=recipient_users, verb="A MAINTENANCE LOG WITH AFFECTING BAGGING IS RECORDED")
+        print("A MAINTENANCE LOG WITH AFFECTING BAGGING IS RECORDED")
