039082c171f2c334cd393ee8b57655447070158f
Francis Carlo Tadena <franciscarlotadena@yahoo.com.ph>
2024-01-28 14:56:00 +0800

    Initialize datams, app for handling reporting, data analytics and more

---
+++ rmics/datams/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DatamsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'datams'
--- rmics/rmics/settings.py
+++ rmics/rmics/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'drom',
     'user',
     'notis',
+    'datams',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
